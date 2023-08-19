00eb7c69ba1caee3ebe0a9fe2b0bc5f121fde15d
Aditya Kumar Vishwakarma <aditya.vishwakarma@intrcity.com>
2023-08-20 01:09:20 +0530

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Task.create(title: "Buy Milk", description: "Buy milk from Dairy")
+Task.create(title: "Go to Gym", description: "Exercise")
+Task.create(title: "Finish Assignment", description: "Finish intern assignment by Xcelyst")
+Task.create(title: "Do Loundry")
+Task.create(title: "Finish Podcast")
+Task.create(title: "Eat Breakfast", completed: true)
+
